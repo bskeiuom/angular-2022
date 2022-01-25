1cf796c096ea81116daa3019fe937b9d5a88bb47
bskeiuom <keiuom.bs23@gmail.com>
2022-01-25 13:31:14 +0600

    acces child prop using ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component, VERSION } from '@angular/core';
+import { AfterViewInit, ViewChild } from '@angular/core';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   name = 'Angular ' + VERSION.major;
   names = ['Dr', ' ', '  Bambasto '];
+
+  @ViewChild(CountdownTimerComponent)
+  private timerComponent!: CountdownTimerComponent;
+
+  seconds() {
+    return 0;
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => (this.seconds = () => this.timerComponent.seconds), 0);
+  }
+
+  start() {
+    this.timerComponent.start();
+  }
+  stop() {
+    this.timerComponent.stop();
+  }
 }
