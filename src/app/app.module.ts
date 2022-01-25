aa6f0074384915d34d770d2364b99c283e5fadf6
bskeiuom <keiuom.bs23@gmail.com>
2022-01-25 14:19:15 +0600

    using rxjs subject shared property between child and parent component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,21 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ChildComponentComponent } from './child-component/child-component.component';
 import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+import { MissioncontrolComponent } from './missioncontrol/missioncontrol.component';
+import { AstronautComponent } from './astronaut/astronaut.component';
+import { MissionService } from './mission.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ChildComponentComponent, CountdownTimerComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    ChildComponentComponent,
+    CountdownTimerComponent,
+    MissioncontrolComponent,
+    AstronautComponent,
+  ],
+  providers: [MissionService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
