3d1de097b76611ba1482abab44f50393e7e8f236
bskeiuom <keiuom.bs23@gmail.com>
2022-01-25 17:50:55 +0600

    1 file modified

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -2,22 +2,21 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child-component',
-  templateUrl: './child-component.component.html',
-  styleUrls: ['./child-component.component.css']
+  template: `<p>{{name}}</p>`,
+  styleUrls: ['./child-component.component.css'],
 })
 export class ChildComponentComponent implements OnInit {
-  
-@Input()
-get name(): string {return this._name;}
-set name(name: string) {
-  this._name = (name && name.trim()) || '<no name set>';
-}
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input()
+  get name(): string {
+    return this._name;
+  }
+  set name(name: string) {
+    this._name = (name && name.trim()) || '<no name set>';
   }
 
-  private _name = '';
+  constructor() {}
 
+  ngOnInit() {}
+
+  private _name = '';
 }
